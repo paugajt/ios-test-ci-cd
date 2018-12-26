0da61e8c003d98f5ef376b3f15779249dd91852e
justin.pauga <justin.pauga@homeadvisor.com>
2018-12-26 10:25:05 -0700

    Initial Commit

---
+++ HAInterview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HAInterview
+//
+//  Created by Pauga, Justin on 12/26/18.
+//  Copyright © 2018 Pauga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
